3ecb9f8a8ba987b8bed2adc6522d6a8e59e43a94
thibault <tcardaire@sophiagenetics.com>
2021-12-18 00:36:54 +0100

    final version

--- app.py
+++ app.py
@@ -9,17 +9,17 @@ app.secret_key = os.environ['FLASK_SECRET_KEY']
 
 client = MongoClient(os.environ['MONGODB_URL'])
 toilets = client.condecrotte.toilets
-toilets.replace_one({"id": "toilet-w-1"}, {"id": "toilet-w-1", "name": "Toilette Femme 1", "available": True}, upsert=True)
-toilets.replace_one({"id": "toilet-w-2"}, {"id": "toilet-w-2", "name": "Toilette Femme 2", "available": True}, upsert=True)
-toilets.replace_one({"id": "toilet-w-3"}, {"id": "toilet-w-3", "name": "Toilette Femme 3", "available": True}, upsert=True)
-toilets.replace_one({"id": "toilet-w-4"}, {"id": "toilet-w-4", "name": "Toilette Femme 4", "available": True}, upsert=True)
-toilets.replace_one({"id": "toilet-w-5"}, {"id": "toilet-w-5", "name": "Toilette Femme 5 (Handicap)", "available": True}, upsert=True)
+toilets.replace_one({"id": "toilet-w-1"}, {"id": "toilet-w-1", "name": "Toilette Femme 1"}, upsert=True)
+toilets.replace_one({"id": "toilet-w-2"}, {"id": "toilet-w-2", "name": "Toilette Femme 2"}, upsert=True)
+toilets.replace_one({"id": "toilet-w-3"}, {"id": "toilet-w-3", "name": "Toilette Femme 3"}, upsert=True)
+toilets.replace_one({"id": "toilet-w-4"}, {"id": "toilet-w-4", "name": "Toilette Femme 4"}, upsert=True)
+toilets.replace_one({"id": "toilet-w-5"}, {"id": "toilet-w-5", "name": "Toilette Femme 5 (Handicap)"}, upsert=True)
 
-toilets.replace_one({"id": "toilet-m-1"}, {"id": "toilet-m-1", "name": "Toilette Homme 1", "available": True}, upsert=True)
-toilets.replace_one({"id": "toilet-m-2"}, {"id": "toilet-m-2", "name": "Toilette Homme 2", "available": True}, upsert=True)
-toilets.replace_one({"id": "toilet-m-3"}, {"id": "toilet-m-3", "name": "Toilette Homme 3", "available": True}, upsert=True)
-toilets.replace_one({"id": "toilet-m-4"}, {"id": "toilet-m-4", "name": "Toilette Homme 4", "available": True}, upsert=True)
-toilets.replace_one({"id": "toilet-m-5"}, {"id": "toilet-m-5", "name": "Toilette Homme 5 (Handicap)", "available": True}, upsert=True)
+toilets.replace_one({"id": "toilet-m-1"}, {"id": "toilet-m-1", "name": "Toilette Homme 1"}, upsert=True)
+toilets.replace_one({"id": "toilet-m-2"}, {"id": "toilet-m-2", "name": "Toilette Homme 2"}, upsert=True)
+toilets.replace_one({"id": "toilet-m-3"}, {"id": "toilet-m-3", "name": "Toilette Homme 3"}, upsert=True)
+toilets.replace_one({"id": "toilet-m-4"}, {"id": "toilet-m-4", "name": "Toilette Homme 4"}, upsert=True)
+toilets.replace_one({"id": "toilet-m-5"}, {"id": "toilet-m-5", "name": "Toilette Homme 5 (Handicap)"}, upsert=True)
 
 # Index page
 @app.route('/', methods=['GET', 'POST'])
@@ -31,24 +31,29 @@ def index():
     # Connect DB
     client = MongoClient(os.environ['MONGODB_URL'])
     toilets = client.condecrotte.toilets
+    reservations = client.condecrotte.reservations
 
     # Handle reservation
     if request.method == 'POST':
-        session['username'] = request.form['username']
+        for id, hour in request.form.items():
+            reservations.insert_one({ "date": date.today().strftime("%Y/%M/%d"), "id": id[5:], "time": hour })
 
     # Load plan
     file = open('static/image/plan.svg', mode='r')
     plan = file.read()
     file.close()
 
+    # Load toilet
+    toilets_list = list(toilets.find())
+
     # Load reservation
-    reservations = client.condecrotte.reservations
-    now = datetime.now()
+    for toilet in toilets_list:
+        toilet["disabled"] = list(reservations.find({ "date": date.today().strftime("%Y/%M/%d"), "id": toilet["id"] }))
 
     return render_template("index.html",
                            date=datetime.today().strftime('%I:%M %p, %a/%-d/%b/%Y'),
                            user=session['username'],
-                           toilets=list(toilets.find()),
+                           toilets=toilets_list,
                            plan=Markup(plan))
 
 @app.route('/login', methods=['GET', 'POST'])
